cc614a1302d36c5757f12d3190f727744f84410d
petryshynNaz <161445887+petryshynNaz@users.noreply.github.com>
2024-02-27 11:21:37 +0200

    Add files via upload

---
+++ Практична№1.py
@@ -0,0 +1,12 @@
+import math
+
+a1 = int(input("¬вед≥ть a1: "))
+a2 = int(input("¬вед≥ть a2: "))
+b1 = int(input("¬вед≥ть b1: "))
+b2 = int(input("¬вед≥ть b2: "))
+m = int(input("¬вед≥ть m: "))
+
+
+A = a1 * b1 - a2 * b2 / (m * a1 - a2 ** 2)
+
+print("«наченн€ A =", A)
